439d454eb79a7aa11ab9d3e60399cb4d0702f1ee
PSILVA999 <p.silva0399@gmail.com>
2022-12-14 13:53:13 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # sushi
- Come and get your sushi
+  Come and get your sushi
+
+  __npm init vite__
+
+  __npm i react-router-dom__
+  __npm install react-scroll__
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import reactLogo from './assets/react.svg'
+
+function App() {
+
+  return (
+    <div>
+      hello
+    </div>
+  )
+}
+
+export default App
